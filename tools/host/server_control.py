4310b9615067f600a139b47ea0a3a947a8d89249
James Northway <jnorthway@sierrawireless.com>
2020-06-10 11:05:43 -0700

    Add udp server script
    Adds udp_server.py
    
    Resolves: LETEST-4681
    Change-Id: I9e66f25e3e810a0615fe6fea85514516253df970

--- tools/host/server_control.py
+++ tools/host/server_control.py
@@ -9,6 +9,7 @@ import flog
 
 
 SERVER_ROOT = os.path.expandvars("$FLAKE_SERVER")
+SERVER_TOOLS = os.path.expandvars("$FLAKE_TOOLS")
 
 
 class ServerAction(Enum):
@@ -23,8 +24,9 @@ class Server:
 
     def __init__(self):
         """Initialize server handler."""
-        global SERVER_ROOT
+        global SERVER_ROOT, SERVER_TOOLS
         self.server_root = SERVER_ROOT
+        self.server_tools = SERVER_TOOLS
         self.config = os.path.join(self.server_root, "nginx.conf")
 
     def test_config(self):
@@ -34,6 +36,7 @@ class Server:
 
     def configure(self, force=False):
         """Configure server at startup."""
+        flog.info("---- Configuring Flake ----")
         assert self.add_files(force), "Failed to add files."
         flog.info("---- Successfully added files ----")
         assert port_publisher.publish_ports(), "Failed to publish ports."
@@ -42,6 +45,8 @@ class Server:
             traffic_manager.configure_server_rules()
         ), "failed to configure traffic rules."
         flog.info("---- Successfully configured traffic rules ----")
+        assert self.start_udp_server(), "failed to start UDP server."
+        flog.info("---- Successfully started UDP server ----")
         return True
 
     def add_files(self, force=False):
@@ -52,6 +57,12 @@ class Server:
         except Exception as e:
             flog.error(e)
             return False
+
+    def start_udp_server(self):
+        """Start udp server as background process."""
+        script_path = os.path.join(self.server_tools, "host/udp_server.py")
+        cmd = "python3 {} &".format(script_path)
+        return os.system(cmd) == 0
 
     def run_action(self, action, config_file=None, force=False):
         """Run action."""
---
+++ tools/host/udp_server.py
@@ -0,0 +1,71 @@
+import os
+import time
+from socket import socket, AF_INET, SOCK_DGRAM
+import flog
+from config_handler import ConfigHandler
+
+
+CONFIG = "$FLAKE_TOOLS/host/config/data_files.json"
+
+
+class UdpServer:
+    """UDP server class."""
+
+    def __init__(self, config):
+        """Initialize udp server."""
+        self.config = config
+        self.port = int(self.config.server["udp"]["port"])
+        self.buffer = int(self.config.server["udp"]["buffer"])
+        self.root = os.path.expandvars("$FLAKE_SERVER/public")
+        self.udp_server = socket(AF_INET, SOCK_DGRAM)
+        self.local_ip = "127.0.0.1"
+
+    def get_file(self, path):
+        """Parse path and return file."""
+        for file_type in self.config.keys():
+            if file_type in path:
+                path = os.path.join(self.root, "files", path)
+                break
+        else:
+            path = os.path.join(self.root, path)
+        if not os.path.exists(path):
+            return b"Incorrect path {}".format(path)
+        with open(path, "rb") as f:
+            return f.read()
+
+    def send_file(self, data, addr):
+        """Send file.
+
+        File must be split into smaller packets and sent."""
+        data_size = len(data)
+        print(data_size)
+        increments = 1024
+        for section in range(0, data_size, increments):
+            self.udp_server.sendto(data[section : (section + increments)], addr)
+            # Sleep between packet sends
+            time.sleep(0.001)
+
+    def run(self):
+        """Run UDP server."""
+        # bind udp socket
+        flog.debug("starting udp server on {}".format(self.port))
+        self.udp_server.bind((self.local_ip, self.port))
+        while True:
+            # listen for udp
+            data, addr = self.udp_server.recvfrom(self.port)
+            flog.debug("Received UDP: {}".format(data))
+            if data:
+                request = data.decode("utf-8")
+                data = self.get_file(path=request)
+                self.send_file(data, addr)
+
+
+def run_server(config_file=CONFIG):
+    """Run udp server."""
+    config = ConfigHandler(config_file)
+    udp_server = UdpServer(config)
+    udp_server.run()
+
+
+if __name__ == "__main__":
+    run_server()
